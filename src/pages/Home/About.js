3562ffb77c6c3025a0b1255d53a18c262b0d1786
Abir Das <abirdas422@gmail.com>
2022-11-11 16:12:44 +0600

    About us done

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import pic from '../../chef_pic.png';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
-        </div>
+        <Row className='align-items-center my-3'>
+            <h4 className='text-center'>About Us</h4>
+
+            <Col md={8}>
+                <p className='p-5 bg-light fw-bold rounded'>
+                    Food Melodies is a platform that connects home chefs and households seamlessly by building an ecosystem. Our professionals are handpicked to serve you hygienic and wholesome meals catered to your taste!
+                </p>
+            </Col>
+            <Col md={4} className="text-center">
+                <img alt="" src={pic} className="rounded me-2 img-fluid w-75" />
+            </Col>
+        </Row>
     );
 };
 
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -3,8 +3,10 @@ import { FaRegCopyright } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div className='text-center my-3 fixed-bottom'>
-            <FaRegCopyright></FaRegCopyright> Developed By Abir Das
+        <div className='text-center mt-3 py-3'>
+
+            <span className=''><FaRegCopyright></FaRegCopyright> Developed By Abir Das</span>
+
         </div>
     );
 };
