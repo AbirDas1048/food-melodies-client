76134adc32a0fd4e8e5a7a8bd92d66687872bfca
Abir Das <abirdas422@gmail.com>
2022-11-11 16:43:36 +0600

    add extra service section

---
+++ src/pages/Home/ExtraService.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import deliveryPic from './../../delivey_pic.png';
+
+const ExtraService = () => {
+    return (
+        <Row className='align-items-center my-3 justify-content-center'>
+            <h4 className='text-center my-3'>Extra Service</h4>
+
+            <Col md={4} className="p-5 bg-light fw-bold rounded text-center">
+                <img src={deliveryPic} alt="delivery pic" className='w-75 img-fluid' />
+                <p>
+                    Fastest home delivery service with cash on payment system.
+                </p>
+            </Col>
+        </Row>
+    );
+};
+
+export default ExtraService;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,17 +3,18 @@ import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import ServiceCard from '../Service/ServiceCard';
 import About from './About';
+import ExtraService from './ExtraService';
 import Slider from './Slider';
 
 const Home = () => {
     const [services, setServices] = useState([]);
     const limit = 3;
 
-    useEffect( () => {
+    useEffect(() => {
         const uri = `http://localhost:5000/services?limit=${limit}`;
         fetch(uri)
-        .then(res => res.json())
-        .then(data => setServices(data))
+            .then(res => res.json())
+            .then(data => setServices(data))
     }, []);
 
     //console.log(services);
@@ -26,17 +27,18 @@ const Home = () => {
 
             <Row xs={1} md={3} sm={2} className="g-4">
                 {
-                    services.map( service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
                 }
             </Row>
 
             <div className='text-center my-3'>
-            <NavLink to="/services">
-                            <Button variant='dark'>All Services</Button>
-                        </NavLink>
+                <NavLink to="/services">
+                    <Button variant='dark'>All Services</Button>
+                </NavLink>
             </div>
-            
+
             <About></About>
+            <ExtraService></ExtraService>
         </Container>
     );
 };
